591eceaafffc9f13ec32f56c47792f1ba38bf99d
torry brelsford <torrybr@rams.colostate.edu>
2017-11-07 14:20:11 -0700

    read teh input file to graph + implement kruskal so I have correct tree

--- assign_8/src/prim_mst.py
+++ assign_8/src/prim_mst.py
@@ -1,8 +1,112 @@
 import heapq
 import random
+import sys
+import copy
+from collections import defaultdict
+from . import kruskal_mst_reference_implementation as impl
+
+
+################################### undirected_graph.py #############################################################
+def normalize_attributes(attributes):
+    if attributes == None:
+        attributes = {}
+    if not isinstance(attributes, (dict, set)):
+        raise ValueError('attributes must be a dict or a set')
+    return attributes
+
+
+class Graph:
+    """An object to represent undirected graphs. Create one like:
+    g = Graph()
+    Then add nodes and edges to it like:
+    g.add_edge("cat", "dog")
+    g.add_node("mouse")
+    You can associate arbitrary information with the nodes and edges like:
+    g.add_edge("FC", "Denver", {"distance in miles": 53})
+    Check out the other methods it defines.
+    Feel free to modify it to your liking."""
+
+    def __init__(self):
+        self.nodes = {}
+        self.edges = defaultdict(dict)
+
+    def __repr__(self):
+        return str({'nodes': self.nodes, 'edges': self.edges})
+
+    def difference(self, other):
+        """Try to report a specific difference between the two graphs,
+        in a format that a human could understand to assist in debugging."""
+        edge_set = set(self.get_edges())
+        other_edge_set = set(other.get_edges())
+        diff = edge_set ^ other_edge_set
+        if diff:
+            return diff
+        for v, u in edge_set:
+            if self.attributes_of(v, u) != other.attributes_of(v, u):
+                return {(v, u): [self.attributes_of(v, u), other.attributes_of(v, u)]}
+        return []
+
+    def __eq__(self, other):
+        return not bool(self.difference(other))
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def copy(self):
+        ret = Graph()
+        ret.nodes = copy.deepcopy(self.nodes)
+        ret.edges = copy.deepcopy(self.edges)
+        return ret
+
+    def add_node(self, identity, attributes=None):
+        self.nodes[identity] = normalize_attributes(attributes)
+
+    def ensure_node(self, identity):
+        if identity not in self.nodes:
+            self.add_node(identity)
+
+    def add_edge(self, identity1, identity2, attributes=None):
+        attributes = normalize_attributes(attributes)
+        self.ensure_node(identity1)
+        self.ensure_node(identity2)
+        self.edges[identity1][identity2] = attributes
+        self.edges[identity2][identity1] = attributes
+
+    def get_nodes(self):
+        return self.nodes.keys()
+
+    def get_edges(self):
+        return ((a, b) for a in self.edges for b in self.edges[a])
+
+    def neighbors(self, identity):
+        if identity in self.edges:
+            return self.edges[identity].keys()
+        return set()
+
+    def attributes_of(self, identity, identity2=None):
+        """Retrieve the attribute map (or set) associated with the arguments.
+        Can be called with a single node, or with two nodes, which represents
+        the edge between them."""
+        if identity2 == None:
+            return self.nodes[identity]
+        return self.edges[identity][identity2]
 
 
 ########################## Start helper files #############################################
+
+def read_lines(filename):
+    with open(filename) as f:
+        return f.readlines()
+
+
+def parse_line(line):
+    return [x for x in line.split()]
+
+
+def read_by_row(filename):
+    lines = read_lines(filename)
+    return [parse_line(line) for line in lines]
+
 
 def name(i):
     return 'v' + hex(i)[2:]
@@ -70,11 +174,37 @@ def write_tree_edges_to_file(edges, filename):
 def compute_mst(filename):
     '''Use Prim's algorithm to compute the minimum spanning tree of the weighted undirected graph
     described by the contents of the file named filename.'''
-    tree_edges = []
-    # TODO compute the edges of a minimum spanning tree
-    write_tree_edges_to_file(tree_edges, filename + '.mst')
+    # tree_edges = []
+
+    # Need to keep track of two graphs
+    input_graph = Graph()
+    mst_graph = Graph()
+
+    # Read in the file
+    data = read_by_row(filename)
+
+    # Add data to graph object
+    for x in data:
+        attribute = {'Weight': int(x[2])}
+        input_graph.add_node(x[0])
+        input_graph.add_edge(x[0], x[1], attribute)
+
+    input_graph_edges = list(input_graph.get_edges())
+    for edge in input_graph_edges:
+        print(edge[0], edge[1], input_graph.attributes_of(edge[0], edge[1]))
+
+
+
+
+
+
+
+        # TODO compute the edges of a minimum spanning tree
+        # write_tree_edges_to_file(tree_edges, filename + '.mst')
 
 
 if __name__ == '__main__':
     # generate graph
-    generate_mst_input()
+    # write_graph_edges_to_file("small_input.txt", generate_weighted_undirected_graph_edges(5, 0))
+    filename = sys.argv[1]
+    compute_mst(filename)
